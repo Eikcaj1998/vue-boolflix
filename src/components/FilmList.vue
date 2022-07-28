4f7ec6486436692532b55bbd87e7eb78b533d64b
Eikcaj.1998 <jw220998@gmail.com>
2022-07-28 17:33:07 +0200

    v if-flag

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -2,13 +2,34 @@
     <ul>
         <li>{{filmApi.title}}</li>
         <li>{{filmApi.original_title}}</li>
-        <li>{{filmApi.original_language}}</li>
+        <li>
+            <img
+              v-if="languages.includes(singleFilm.original_language)"
+              :src="
+                require(`../assets/flags/${singleFilm.original_language}.png`)
+              "
+              alt="language flag"
+            />
+            <img v-else :src="require(`../assets/flags/rsw.png`)" alt="">
+          </li>
         <li>{{filmApi.vote_average}}</li>
     </ul>
 </template>
 
 <script>
 export default {
+
+     data: function () {
+    return {
+      languages: [
+        "en",
+        "it",
+      ],
+
+
+    };
+  },
+
     props:["filmApi"]
 }
 </script>
