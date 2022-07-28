d1ed9e9630ffcd8f404455585c0dba70eccc7d21
Eikcaj.1998 <jw220998@gmail.com>
2022-07-28 15:36:42 +0200

    header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,27 @@
+<template>
+<form role="search">
+    <input
+        placeholder="Cerca Film o Serie TV"
+        v-model="searchedMovie"
+    />
+    <button type="submit">
+        Cerca
+    </button>
+</form>
+</template>
+<script>
+export default {
+  data: function(){
+    return{
+      searchedMovie:'',
+    }
+  },
+
+  methods:{
+    
+  }
+};
+</script>
+
+<style>
+</style>
