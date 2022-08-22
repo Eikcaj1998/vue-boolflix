714af6c97500a75d1c2674b2962ba90847daea21
Eikcaj.1998 <jw220998@gmail.com>
2022-08-22 15:37:40 +0200

    img blank

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -1,9 +1,5 @@
 <template>
     <ul>
-        <li>
-         <img 
-         :src="`https://image.tmdb.org/t/p/w342${singleFilmApi.poster_path}`">
-        </li>
         <li>{{singleFilmApi.title}}</li>
         <li>{{singleFilmApi.original_title}}</li>
         <li>
@@ -14,11 +10,20 @@
               alt="language flag"
             />
           </li>
-        <li>{{singleFilmApi.vote_average}}</li>
+        <li>
+         <img
+         :src="cover"
+         :alt="singleFilmApi.title">
+
+        </li>
+        <li><i class="fa-solid fa-star"></i>{{singleFilmApi.vote_average}}</li>
     </ul>
 </template>
 
 <script>
+  const imgBaseUrl = "https://image.tmdb.org/t/p/";
+  const imgSize = "w342";
+  const coverPlaceholder="https://www.altavod.com/assets/images/poster-placeholder.png";
 export default {
 
      data: function () {
@@ -28,13 +33,16 @@ export default {
         "it",
       ],
 
-      imgBaseUrl: "https://image.tmdb.org/t/p/",
-      imgSize: "w342",
 
     };
 
   },
-
+    computed: {
+      cover(){
+        if(!this.singleFilmApi.poster_path) return coverPlaceholder;
+       return imgBaseUrl+imgSize+this.singleFilmApi.poster_path
+      }
+    },
     props:["singleFilmApi"]
 }
 </script>
--- src/components/TvSeries.vue
+++ src/components/TvSeries.vue
@@ -1,20 +1,24 @@
 <template>
     <ul>
-        <li>
-        <img 
-        :src="`https://image.tmdb.org/t/p/w342${singleTvSeries.poster_path}`">
-        </li>
         <li>{{ singleTvSeries.name }}</li>
         <li>{{ singleTvSeries.original_name }}</li>
         <li>
         <img class="language"
         :src="require(`../assets/flags/${singleTvSeries.original_language}.png`)" 
         alt="language flag"></li>
+        <li>
+        <img
+         :src="cover"
+         :alt="singleTvSeries.title">
+        </li>
         <li>{{singleTvSeries.vote_average}}</li>
     </ul>
 </template>
 
 <script>
+const imgBaseUrl = "https://image.tmdb.org/t/p/";
+const imgSize = "w342";
+const coverPlaceholder="https://www.altavod.com/assets/images/poster-placeholder.png";
 export default {
 
      data: function () {
@@ -29,7 +33,12 @@ export default {
 
     };
   },
-
+  computed: {
+      cover(){
+        if(!this.singleTvSeries.poster_path) return coverPlaceholder;
+       return imgBaseUrl+imgSize+this.singleTvSeries.poster_path
+      }
+    },
     props:["singleTvSeries"]
 }
 </script>
