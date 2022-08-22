ad3dd647bd9f7f1b841af4a55435fb596ede06e9
Eikcaj.1998 <jw220998@gmail.com>
2022-08-22 17:40:45 +0200

    the-main scss

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,15 +1,30 @@
 <template>
     <main>
-        <FilmList
-        v-for="singleFilmApi in filmApi"
-        :key="singleFilmApi.id"
-        :singleFilmApi="singleFilmApi"
-        />
-        <TvSeries
-        v-for="singleTvSeries in seriesTvApi"
-        :key="singleTvSeries.id"
-        :singleTvSeries="singleTvSeries"
-        />
+      <div class="container-fluid">
+        <div class="row">
+          <h1 class="text-light fw-bold text-center mt-3">Films</h1>
+          <div class="col-12 d-flex flex-wrap justify-content-between">
+          <FilmList
+          class="mt-4 film-list"
+          v-for="singleFilmApi in filmApi"
+          :key="singleFilmApi.id"
+          :singleFilmApi="singleFilmApi"
+          />
+          </div>
+        </div>
+      <hr class="text-light mt-5">
+        <div class="row">
+          <h1 class="text-light fw-bold text-center mt-3">Series</h1>
+          <div class="col-12 d-flex flex-wrap justify-content-between">
+            <TvSeries
+            class="mt-4 tv-series"
+            v-for="singleTvSeries in seriesTvApi"
+            :key="singleTvSeries.id"
+            :singleTvSeries="singleTvSeries"
+            />
+          </div>
+      </div>
+    </div>
 
     </main>
 </template>
@@ -27,5 +42,20 @@
     };
     </script>
     
-    <style>
-    </style>
+<style lang="scss">
+ul {
+  list-style: none;
+}
+
+main {
+  background-color: #1b1b1b;
+}
+
+.film-list,
+.tv-series {
+  width: calc((100% / 3) - 3rem);
+  padding: 2rem;
+}
+
+
+</style>
